e1c209b09497807fa9f49a37c073bafecea312e9
Inquallity <inquallity@gmail.com>
2020-09-17 21:24:27 +0300

    #common:rm build folders

--- entities/scanner/build/tmp/kapt3/stubs/debug/com/inquallity/daggersandbox/scanner/IScanner.java
+++
@@ -1,9 +0,0 @@
-package com.inquallity.daggersandbox.scanner;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0010\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\bf\u0018\u00002\u00020\u0001J\b\u0010\u0002\u001a\u00020\u0003H&\u00a8\u0006\u0004"}, d2 = {"Lcom/inquallity/daggersandbox/scanner/IScanner;", "", "doScan", "", "scanner_debug"})
-public abstract interface IScanner {
-    
-    public abstract void doScan();
-}
