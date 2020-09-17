3a816981e05fec154964da8c253d2cd778132e4c
Inquallity <inquallity@gmail.com>
2020-09-17 21:34:38 +0300

    #common:added app

---
+++ app/build/tmp/kapt3/stubs/debug/com/inquallity/daggersandbox/AppKt.java
@@ -0,0 +1,12 @@
+package com.inquallity.daggersandbox;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\f\n\u0000\n\u0002\u0010\b\n\u0002\u0010\u000e\n\u0000\u001a\r\u0010\u0000\u001a\u00020\u0001*\u00020\u0002H\u0086\u0002\u00a8\u0006\u0003"}, d2 = {"unaryPlus", "", "", "app_debug"})
+public final class AppKt {
+    
+    public static final int unaryPlus(@org.jetbrains.annotations.NotNull()
+    java.lang.String $this$unaryPlus) {
+        return 0;
+    }
+}
